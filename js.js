e00ecaba1efd87df52e576fa70f8c3e24433a378
MrLeonKeNN <61803026+MrLeonKeNN@users.noreply.github.com>
2020-06-18 20:45:49 +0300

    !!

--- js.js
+++ js.js
@@ -1,4 +1,19 @@
-function zero(){
-	
-	
-}
+ //document.getElementById('ilove').onclick = zeroTwo();
+
+ //function zeroTwo(){
+	// var a = document.getElementById('h1');
+	// a.style.color = 'black';
+ //}
+
+
+ $(document).ready(function() {
+	 $('.iLove').click(function(event){
+		 $('.got').toggleClass('active');
+		 $('body').toggleClass('lock');
+		 
+	 });
+ });
+ $('.header__list').click(function(event) {
+	$('.header__burger,.header__menu').removeClass('active');
+	$('body').removeClass('lock');
+})
