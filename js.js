59078ab8b05a211a65055f426b04a52dc1a28c14
MrLeonKeNN <61803026+MrLeonKeNN@users.noreply.github.com>
2020-06-24 10:22:30 +0300

    fix animated

--- js.js
+++ js.js
@@ -17,3 +17,12 @@
 	$('.header__burger,.header__menu').removeClass('active');
 	$('body').removeClass('lock');
 })
+
+$("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
